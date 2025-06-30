975b13270f0164c892d74929507d104802a27151
Dadang Eka Putra <88359335+ekaputra012309@users.noreply.github.com>
2025-06-30 15:00:54 +0700

    Update AppServiceProvider.php
    update public path for hosting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Dompdf\Dompdf;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +15,21 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
+        $this->app->bind('dompdf', function ($app) {
+            $options = $app->make('dompdf.options');
+            $dompdf = new Dompdf($options);
+    
+            // Custom public path pointing to your real public_html
+            $customPublicPath = realpath(base_path('../public_html'));
+    
+            if ($customPublicPath === false) {
+                throw new \RuntimeException('Cannot resolve custom public path');
+            }
+    
+            $dompdf->setBasePath($customPublicPath);
+    
+            return $dompdf;
+        });
     }
 
     /**
